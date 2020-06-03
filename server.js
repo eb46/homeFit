df687c6e3862adf572c6956a936849acd171673d
eb46 <ericjbondoc@gmail.com>
2020-06-03 08:54:02 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,41 @@
+// Dependencies
+const express = require('express')
+const methodOverride = require('method-override')
+const mongoose = require('mongoose')
+const app = express()
+const db = mongoose.connection
+require('dotenv').config()
+
+// Port
+const PORT = process.env.PORT || 3333
+
+// Database
+const MONGODB_URI = process.env.MONGODB_URI
+
+// Connect to Mongo &
+// Fix Depreciation Warnings from Mongoose
+// May or may not need these depending on your Mongoose version
+mongoose.connect(MONGODB_URI , { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }
+);
+
+// Error / success
+db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
+db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
+db.on('disconnected', () => console.log('mongo disconnected'));
+
+
+// Middleware
+// use the public folder
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:false}))
+app.use(methodOverride('_method'))
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+// Listener
+app.listen(PORT, () => {
+  console.log(`I'm listening on port: ${PORT}`);
+})
